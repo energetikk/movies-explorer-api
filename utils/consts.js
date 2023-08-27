65bcdc1a53b3b2115e4d23f68e5a0a7883c287e5
Pavel Demin <pavlikdemin@yandex.ru>
2023-08-28 02:29:03 +0300

    Версия для деплоя на сервер

--- utils/consts.js
+++ utils/consts.js
@@ -2,5 +2,5 @@
 // eslint-disable-next-line no-useless-escape
 // const linkRegular = /^(http|https):\/\/(?:www\.)?[a-zA-Z0-9._~\-:?#[\]@!$&'()*+,\/;=]{2,256}\.[a-zA-Z0-9.\/?#-]{2,}$/;
 const linkRegular = /^(https?:\/\/)?[^\s]*\.(jpg|jpeg|png|gif|bmp|test)$/;
-const linkRegularMovie = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be|rutube\.ru)\/(.+)$/;
-module.exports = { linkRegular, linkRegularMovie };
+const linkRegularMovies = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be|rutube\.ru)\/(.+)$/;
+module.exports = { linkRegular, linkRegularMovies };
